99666088367d7876da46076b84d363fa6298ce7c
martinim86 <vano4461@gmail.com>
2023-05-29 13:28:31 +0300

    Update Weather.php

--- app/Models/Weather.php
+++ app/Models/Weather.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Weather extends Model
 {
     use HasFactory;
-    public $api_key_openweathermap = 'ec7de5a75a1b24401ec1257d6cdc7e6c';
-    public $api_key_weatherapi = '7006a061fb48480ea80161901232605 ';
+    public $api_key_openweathermap = '';
+    public $api_key_weatherapi = '';
 }
 
